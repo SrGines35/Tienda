04b21c0d6b1da5cc28c79df089edc77605ae92c5
byOscarPINE <emanuelsant768@gmail.com>
2026-05-27 08:32:30 -0600

    Feat(products): Se agrega la opcion de anadir productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Products } from './pages/products/products/products';
 
 export const routes: Routes = [
     
 {
     path: 'home',
     component: Home
+   },
+   {
+    path: 'products',
+    component: Products
+   },
+   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
    }
-
 ];
